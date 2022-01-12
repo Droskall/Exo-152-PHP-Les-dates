941778a75368f699ccf8fefabc4e37b20d472373
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-01-12 10:23:05 +0100

    fini

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
 // Premiere ligne
+echo date("l")."<br><br>";
 
+// Deuxieme ligne
+echo date("Y.d.m", 1539302400)."<br><br>";
+
+// Troisiéme ligne
+echo date("H:i:s", 1546342507)."<br><br>";
 
 
 
